917f198a7a1b7a2e2fcec9d19daea3344e9a9783
shivaprasad918 <pshivaprasad783@gmail.com>
2024-08-01 12:55:01 +0530

    fix: fix render allproducts url

--- controllers/productController.js
+++ controllers/productController.js
@@ -357,7 +357,7 @@ const getProducts = async (req, res) => {
         const totalPages = Math.ceil(totalProducts / perPage);
         const paginatedProducts = products.slice(skip, skip + perPage);
 
-        res.render('users/allProducts', {
+        res.render('allProducts', {
             currentPage,
             totalPages,
             products: paginatedProducts,
